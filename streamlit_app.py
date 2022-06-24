d29458f40bc91fd5f18f47d047bba48f0ceef32b
tobididit <95889953+tobididit@users.noreply.github.com>
2022-06-24 14:59:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,68 +1,70 @@
-# pip install inquirer
-# from pprint import pprint
 
-# import inquirer
+print ("Hello tobi")
+# # pip install inquirer
+# # from pprint import pprint
 
+# # import inquirer
+
+
+# # questions = [
+# #   inquirer.List('nodename',
+# #                 message="What nodename would you be using do you need?",
+# #                 choices=["gpu-eu-w2a", "gpu-eu-w2b", "pool-infra", "pool-micro-services-cpu", "pool-pulsar"],
+# #             ),
+# # ]
+# # answers = inquirer.prompt(questions)
+# # pprint(answers["choices"])
+
+
+
+
+# numberofnodes = input ("how many nodes are needed?")
 
 # questions = [
-#   inquirer.List('nodename',
-#                 message="What nodename would you be using do you need?",
-#                 choices=["gpu-eu-w2a", "gpu-eu-w2b", "pool-infra", "pool-micro-services-cpu", "pool-pulsar"],
+#   inquirer.List('machinetype',
+#                 message="What machinetype would you be using do you need?",
+#                 choices=["n1-standard-4", "n2-standard-4", "n2-standard-16"],
 #             ),
 # ]
 # answers = inquirer.prompt(questions)
-# pprint(answers["choices"])
+# print (answers["machinetype"])
 
 
 
+# pphinstance  = input ("how many pph/instance?")
 
-numberofnodes = input ("how many nodes are needed?")
+# minuser  = input ("how many min users?")
 
-questions = [
-  inquirer.List('machinetype',
-                message="What machinetype would you be using do you need?",
-                choices=["n1-standard-4", "n2-standard-4", "n2-standard-16"],
-            ),
-]
-answers = inquirer.prompt(questions)
-print (answers["machinetype"])
+# maxuser  = input ("how many max users?")
 
 
+# guessmax  = input ("guess how many max users?")
 
-pphinstance  = input ("how many pph/instance?")
-
-minuser  = input ("how many min users?")
-
-maxuser  = input ("how many max users?")
+# minuserpernode  = input ("minuser / float(numberofnodes)")
 
 
-guessmax  = input ("guess how many max users?")
-
-minuserpernode  = input ("minuser / float(numberofnodes)")
-
-
-users  = input ("guess how many max users?")
+# users  = input ("guess how many max users?")
 
 
 
-questions = [
-  inquirer.List('services',
-                message="What services would you be using?",
-                choices=["polyp-det", "ms-all", "websocket, webserver", "freeze, image storage, prediction storage", "pulsar"],
-            ),
-]
-answers = inquirer.prompt(questions)
-print (answers["machinetype"])
+# questions = [
+#   inquirer.List('services',
+#                 message="What services would you be using?",
+#                 choices=["polyp-det", "ms-all", "websocket, webserver", "freeze, image storage, prediction storage", "pulsar"],
+#             ),
+# ]
+# answers = inquirer.prompt(questions)
+# print (answers["machinetype"])
 
 
 
-users  = input ("guess how many max users?")
+# users  = input ("guess how many max users?")
 
-runtimedayinhrs  = input ("what is the runtime daily in hours?")
+# runtimedayinhrs  = input ("what is the runtime daily in hours?")
 
-daysperweek  = input ("how many days in a week?")
+# daysperweek  = input ("how many days in a week?")
 
-procedurelength  = input ("procedure length in hrs?")
+# procedurelength  = input ("procedure length in hrs?")
 
 
 
@@ -70,21 +72,21 @@ procedurelength  = input ("procedure length in hrs?")
 
 
 
-print(numberofnodes)
+# print(numberofnodes)
 
 
 
 
 
-nodeneeded = {
-    print (answers["nodename"])
-(users / minuserpernode(100))
+# nodeneeded = {
+#     print (answers["nodename"])
+# (users / minuserpernode(100))
 
-}
+# }
 
-costpermonth = {
-    print (answers["costpermonth"])
-           (nodeneeded * runtimedayinhrs * daysperweek * 4.345 * pphinstance)
+# costpermonth = {
+#     print (answers["costpermonth"])
+#            (nodeneeded * runtimedayinhrs * daysperweek * 4.345 * pphinstance)
 
 
-}
+# }
