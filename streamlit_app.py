79dc9408c55b1aadd3e653bde3d38dbb162653fd
tobididit <95889953+tobididit@users.noreply.github.com>
2022-06-28 13:51:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,20 @@ Nodename = st.selectbox("Nodename: ",
 					['gpu-eu-w2a', 'gpu-eu-w2b', 'pool-infra', 'pool-micro-services-cpu', 'pool-pulsar'])
 
 # print the selected hobby
-st.write("Your hobby is: ", Nodename)
+st.write("Your selected Nodename is: ", Nodename)
+
+
+
+# Text Input
+
+# save the input text in the variable 'name'
+# first argument shows the title of the text input box
+# second argument displays a default text inside the text input area
+numberofnodes = st.text_input("how many nodes are needed?", "Type Here ...")
+
+# display the name when the submit button is clicked
+# .title() is used to get the input text string
+if(st.button('Submit')):
+	result = numberofnodes.title()
+	st.success(result)
 
