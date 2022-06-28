702288a99c9d98567bf286e3eaf014c83c503d57
tobididit <95889953+tobididit@users.noreply.github.com>
2022-06-28 20:18:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,17 +31,6 @@ if(st.button('Submit')):
 	st.success(result)
 	
 	
-	# Selection box
-
-# first argument takes the titleof the selectionbox
-# second argument takes options
-
-	machine = st.selectbox("machine: ",
-					['n1-standard-4', 'n2-standard-4', 'n2-standard-16'])
-
-# print the selected hobby
-st.write("Your selected machine is: ", machine)
-	
 	
 
 	# slider
