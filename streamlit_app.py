779307c22d26b5855e1a12f960e9eaba6461683b
tobididit <95889953+tobididit@users.noreply.github.com>
2022-06-28 13:47:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,15 @@ import streamlit as st
 
 # Title
 st.title("Thanks for choosing to use Odin's Cloud Cost Calculator !!!")
+
+
+# Selection box
+
+# first argument takes the titleof the selectionbox
+# second argument takes options
+Nodename = st.selectbox("Nodename: ",
+					['gpu-eu-w2a', 'gpu-eu-w2b', 'pool-infra', 'pool-micro-services-cpu', 'pool-pulsar'])
+
+# print the selected hobby
+st.write("Your hobby is: ", Nodename)
+
