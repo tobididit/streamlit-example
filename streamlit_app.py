c628a235cfeb6b70aa2ce6bef2a9a96bed15e654
tobididit <95889953+tobididit@users.noreply.github.com>
2022-06-29 02:57:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,22 +23,12 @@ st.write("Your selected MachineType is: ", MT)
 
 
 
-# Text Input
-
-# save the input text in the variable 'name'
-# first argument shows the title of the text input box
-# second argument displays a default text inside the text input area
-numberofnodes = st.text_input("how many nodes are needed?", "Type Here ...")
-
-# display the name when the submit button is clicked
-# .title() is used to get the input text string
-if(st.button('Submit')):
-	result = numberofnodes.title()
-	st.success(result)
 	
 
 	
 	# TAKE WEIGHT INPUT in kgs
+number of nodes = st.number_input("how many nodes are needed?")	
+	
 pphinstance = st.number_input("how many pph/instance?")
 
 maxuser = st.number_input("how many maxuser?")
