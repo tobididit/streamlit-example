4851409f6af423dbdfed565405e6de129b995aa2
tobididit <95889953+tobididit@users.noreply.github.com>
2022-06-30 13:46:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.title("Thanks for choosing to use Odin's Cloud Cost Calculator !!!")
 
 # Selection box
 nodeholder = {}
-with st.form("Calculator"):
+with st.form("NodeToBeUsed"):
 
 	# first argument takes the titleof the selectionbox
 	# second argument takes options
@@ -25,59 +25,60 @@ with st.form("Calculator"):
 		nodeneeded = (users / minuser)
 		st.text("Your Node needed is {}.".format(nodeneeded))
 		nodeholder[Nodename] = nodeneeded
-
-MT = st.selectbox("MachineType: ",
-					 ['n1-standard-4', 'n2-standard-4', 'n2-standard-16'])
-
-
-# print the selected MT
-st.write("Your selected MachineType is: ", MT)
-
-	
-# TAKE WEIGHT INPUT in kgs
-numberofnodes = st.number_input("how many nodes are needed?")	
-	
-pphinstance = st.number_input("how many pph/instance?")
-
-maxuser = st.number_input("how many maxuser?")
-
-
-minuser = st.number_input("how many minuser?")
-
-guessmax = st.number_input("guess how many max users?")
-
-Services = st.selectbox("Services: ",
-					 ['polyp-det', 'ms-all', 'websocket/webserver', 'freeze/image/storage', 'prediction storage', 'pulsar'])
-
-
-# print the selected MT
-st.write("The service you are using is: ", Services)
-	
-	
-users = st.number_input("guess how many users?")
+		
+with st.form("TotalCost"):
 	
 
-	# slider
-
-# first argument takes the title of the slider
-# second argument takes the starting of the slider
-# last argument takes the end number
-hours = st.slider("How many hours in a day", 1, 24)
-
-# print the level
-# format() is used to print value
-# of a variable at a specific position
-st.text('Selected: {}'.format(hours))
-
-days = st.slider("How many days in a week", 1, 7)
-st.text('Selected: {}'.format(days))
+	MT = st.selectbox("MachineType: ",
+						 ['n1-standard-4', 'n2-standard-4', 'n2-standard-16'])
 
 
-procedurelength = st.number_input("What is the proocedure length in hours?")
+	# print the selected MT
+	st.write("Your selected MachineType is: ", MT)
 
 
+	# TAKE WEIGHT INPUT in kgs
+	numberofnodes = st.number_input("how many nodes are needed?")	
+
+	pphinstance = st.number_input("how many pph/instance?")
+
+	maxuser = st.number_input("how many maxuser?")
 
 
-nodeneeded = (users / minuser)
+	minuser = st.number_input("how many minuser?")
 
-st.text("Your Node needed is {}.".format(nodeneeded))
+	guessmax = st.number_input("guess how many max users?")
+
+	Services = st.selectbox("Services: ",
+						 ['polyp-det', 'ms-all', 'websocket/webserver', 'freeze/image/storage', 'prediction storage', 'pulsar'])
+
+
+	# print the selected MT
+	st.write("The service you are using is: ", Services)
+
+
+	users = st.number_input("guess how many users?")
+
+
+		# slider
+
+	# first argument takes the title of the slider
+	# second argument takes the starting of the slider
+	# last argument takes the end number
+	hours = st.slider("How many hours in a day", 1, 24)
+
+	# print the level
+	# format() is used to print value
+	# of a variable at a specific position
+	st.text('Selected: {}'.format(hours))
+
+	days = st.slider("How many days in a week", 1, 7)
+	st.text('Selected: {}'.format(days))
+
+
+	procedurelength = st.number_input("What is the proocedure length in hours?")
+
+	submitted = st.form_submit_button("Submit")
+	if submitted:
+		costpermonth = (nodeneeded * hours * days * 4.345 * pphinstance)
+		st.text("Your cost per month is {}.".format(costpermonth))
