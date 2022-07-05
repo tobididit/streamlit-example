12fd530c715abe6cea9c44ccdb303926844c1bcb
tobididit <95889953+tobididit@users.noreply.github.com>
2022-07-05 16:08:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,7 +85,7 @@ with st.form("costinmonth"):
 	submitted = st.form_submit_button("Submit")
 
 	if submitted:
-		costinmonth = math.ceil((gpueuw2a["nodeneeded"] * 4.345 * i gpueuw2a["pphinstance"]))
+		costinmonth = math.ceil((int(gpueuw2a["nodeneeded"]) * hours * days * 4.345 * int(gpueuw2a["pphinstance"])))
 		st.text("Cost in a month is {}.".format(costinmonth))
 		totalcost[Nodename] = costinmonth
 		st.text("Cost in a month is {}.".format(totalcost))
